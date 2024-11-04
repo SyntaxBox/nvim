6f7bbc00edbf9827f4c3f3a916198f24316df1b6
Abdelhamid Boudjit <qhamid@proton.me>
2024-11-04 13:45:22 +0100

    add comments shortcuts

--- lua/hamid/core/keymaps.lua
+++ lua/hamid/core/keymaps.lua
@@ -30,3 +30,5 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>cc", "<Plug>(comment_toggle_linewise_current)", { desc = "Toggle comment (normal mode)" })
+keymap.set("v", "<leader>cc", "<Plug>(comment_toggle_linewise_visual)", { desc = "Toggle comment (visual mode)" })
